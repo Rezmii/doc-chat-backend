2da2636642ce4217c8f32a531b9261decc46d0aa
Rezmii <bartoszrezmer20@gmail.com>
2025-06-29 17:34:43 +0200

    naprawienie endpointu lekarzy

--- src/api/doctorRoutes.ts
+++ src/api/doctorRoutes.ts
@@ -3,48 +3,42 @@ import prisma from "../lib/prisma";
 
 const router = Router();
 
-router.get(
-  "/api/doctors",
-  async (req: Request, res: Response): Promise<void> => {
-    try {
-      const doctors = await prisma.doctor.findMany({
-        include: {
-          user: true,
-        },
-      });
-      res.json(doctors);
-    } catch (error) {
-      console.error("Błąd podczas pobierania lekarzy:", error);
-      res.status(500).json({ error: "Nie udało się pobrać danych lekarzy." });
-    }
+router.get("/", async (req: Request, res: Response): Promise<void> => {
+  try {
+    const doctors = await prisma.doctor.findMany({
+      include: {
+        user: true,
+      },
+    });
+    res.json(doctors);
+  } catch (error) {
+    console.error("Błąd podczas pobierania lekarzy:", error);
+    res.status(500).json({ error: "Nie udało się pobrać danych lekarzy." });
   }
-);
+});
 
-router.get(
-  "/api/doctors/:id",
-  async (req: Request, res: Response): Promise<void> => {
-    const { id } = req.params;
+router.get("/:id", async (req: Request, res: Response): Promise<void> => {
+  const { id } = req.params;
 
-    try {
-      const doctor = await prisma.doctor.findUnique({
-        where: {
-          id: id,
-        },
-        include: {
-          user: true,
-        },
-      });
+  try {
+    const doctor = await prisma.doctor.findUnique({
+      where: {
+        id: id,
+      },
+      include: {
+        user: true,
+      },
+    });
 
-      if (!doctor) {
-        res.status(404).json({ error: "Nie znaleziono lekarza" });
-      } else {
-        res.json(doctor);
-      }
-    } catch (error) {
-      console.error(`Błąd podczas pobierania lekarza o ID ${id}:`, error);
-      res.status(500).json({ error: "Nie udało się pobrać danych lekarza." });
+    if (!doctor) {
+      res.status(404).json({ error: "Nie znaleziono lekarza" });
+    } else {
+      res.json(doctor);
     }
+  } catch (error) {
+    console.error(`Błąd podczas pobierania lekarza o ID ${id}:`, error);
+    res.status(500).json({ error: "Nie udało się pobrać danych lekarza." });
   }
-);
+});
 
 export default router;
